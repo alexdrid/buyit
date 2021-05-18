6caf5a4825d0a1b1bdb4d217f06664245e2fd75a
alexdrid <alexdrid13@gmail.com>
2021-05-18 20:15:27 +0300

    first commit

---
+++ client/src/components/Header.js
@@ -0,0 +1,81 @@
+import React from "react"
+import { Route } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
+import { LinkContainer } from "react-router-bootstrap"
+import { Navbar, Nav, Container, NavDropdown, Col } from "react-bootstrap"
+import SearchBox from "./SearchBox"
+import { logout } from "../actions/userActions"
+
+const Header = () => {
+  const dispatch = useDispatch()
+
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
+
+  const logoutHandler = () => {
+    dispatch(logout())
+  }
+
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <LinkContainer to="/" className="mr-5">
+            <Navbar.Brand>Buy It</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Col lg={3} className="px-0">
+              <Nav className="ml-auto justify-content-end">
+                <LinkContainer to="/cart">
+                  <Nav.Link>
+                    <i className="fas fa-shopping-cart"></i> Cart
+                  </Nav.Link>
+                </LinkContainer>
+                {userInfo ? (
+                  <NavDropdown title={userInfo.name} id="username">
+                    <LinkContainer to="/profile">
+                      <NavDropdown.Item>Profile</NavDropdown.Item>
+                    </LinkContainer>
+                    <NavDropdown.Item onClick={logoutHandler}>
+                      Logout
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                ) : (
+                  <LinkContainer to="/login">
+                    <Nav.Link>
+                      <i className="fas fa-user"></i> Sign In
+                    </Nav.Link>
+                  </LinkContainer>
+                )}
+                {userInfo && userInfo.isAdmin && (
+                  <NavDropdown title="Admin" id="adminmenu">
+                    <LinkContainer to="/admin/userlist">
+                      <NavDropdown.Item>Users</NavDropdown.Item>
+                    </LinkContainer>
+                    <LinkContainer to="/admin/productlist">
+                      <NavDropdown.Item>Products</NavDropdown.Item>
+                    </LinkContainer>
+                    <LinkContainer to="/admin/orderlist">
+                      <NavDropdown.Item>Orders</NavDropdown.Item>
+                    </LinkContainer>
+                  </NavDropdown>
+                )}
+              </Nav>
+            </Col>
+            <Col
+              lg={9}
+              className="mx-auto px-0 px-lg-3 m-md-0 m-2 order-lg-first"
+            >
+              <Route
+                render={({ history }) => <SearchBox history={history} />}
+              />
+            </Col>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
+}
+
+export default Header
---
+++ client/src/components/SearchBox.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react"
+import { Form, Button, InputGroup, FormControl } from "react-bootstrap"
+
+const SearchBox = ({ history }) => {
+  const [keyword, setKeyword] = useState("")
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    if (keyword.trim()) {
+      history.push(`/search/${keyword}`)
+    } else {
+      history.push("/")
+    }
+  }
+
+  return (
+    <InputGroup onSubmit={submitHandler}>
+      <FormControl
+        placeholder="Search Products"
+        onChange={(e) => setKeyword(e.target.value)}
+        aria-label="Search Products"
+      />
+      <InputGroup.Append>
+        <Button variant="primary" onClick={submitHandler}>
+          Search
+        </Button>
+      </InputGroup.Append>
+    </InputGroup>
+  )
+}
+
+export default SearchBox
